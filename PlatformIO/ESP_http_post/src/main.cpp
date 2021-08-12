d4b6277e35944a2c6df1e0d43b702eb255c51b17
Josef <61586098+JosefGst@users.noreply.github.com>
2021-08-12 09:35:26 +0800

    add http post

---
+++ PlatformIO/ESP_http_post/src/main.cpp
@@ -0,0 +1,103 @@
+#include <Arduino.h>
+
+/*
+  Rui Santos
+  Complete project details at Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-http-get-post-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  Code compatible with ESP8266 Boards Version 3.0.0 or above 
+  (see in Tools > Boards > Boards Manager > ESP8266)
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+const char* ssid = "LSK 7028";
+const char* password = "wesharetechnology";
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://148.70.180.108:8000/lab/hwinfo";
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
+
+
+String buildData(String id, String timestamp, String weight, String unitinfo);
+
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+
+      // // Specify content-type header
+      // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // // Data to send with HTTP POST
+      // String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";           
+      // // Send HTTP POST request
+      // int httpResponseCode = http.POST(httpRequestData);
+      
+      // If you need an HTTP request with a content type: application/json, use the following:
+      http.addHeader("Content-Type", "application/json");
+      String data = buildData("abcdefghij0123456789", "timestamp", "250", "unitinfo");
+      int httpResponseCode = http.POST(data);
+
+      // If you need an HTTP request with a content type: text/plain
+      //http.addHeader("Content-Type", "text/plain");
+      //int httpResponseCode = http.POST("Hello, World!");
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+
+String buildData(String id, String timestamp, String weight, String unitinfo) { //组建发送的json字符串
+  String payload = "{\"unitinfo\":\"testxtray3\",\"data\":{\"weight\":\"";
+  payload += weight;
+  payload += "\",\"uids\":[";
+  payload += id;
+  payload += String("]}}");
+  //Serial.println((String)"payload: " + payload);
+  return payload;
+  //  char attributes[100];
+  //  payload.toCharArray( attributes, 100 );
+}
